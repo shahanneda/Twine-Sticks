da6843ffd2df0235558edafa42c749462e554909
shahanneda <shahan.neda@gmail.com>
2016-02-25 18:03:23 -0800

    Added Bye world

---
+++ Assets/ByeWorld.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ByeWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("byeWorld!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
